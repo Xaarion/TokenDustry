9df68cb0300cdbc7e9eecb89d595424f3d0d857b
Xaarion <Xaarion@laposte.net>
2023-05-02 22:08:15 +0200

    aze

---
+++ src/views/BackOfficeCrypto.vue
@@ -0,0 +1,18 @@
+<template></template>
+          
+    <script>
+// @ is an alias to /src
+
+import { store } from "../store.js";
+
+export default {
+  name: "BackOfficeCrypto",
+
+  data() {
+    return {
+      dataStore: store,
+    };
+  },
+};
+</script>
+          
